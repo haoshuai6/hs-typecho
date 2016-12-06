00996b294ec1aef474cc114a289a8567388d0343
haoshuai_it <haoshuai_it@163.com>
2016-12-06 09:19:25 +0800

    修改文字

--- usr/themes/jianshu/post.php
+++ usr/themes/jianshu/post.php
@@ -67,7 +67,7 @@
             <?php parseContent($this); ?>
         </div>
 		<div class="post-donate">
-			<p><i class="fa fa-rmb"></i> 打赏几块钱，帮我买杯咖啡吧，谢谢大家！<button class="btn s2 fr" onclick="$('#alipay-form').submit();"><i class="fa fa-cny"> <?php _e('打赏支持');?></i></button></p>
+			<p><i class="fa fa-rmb"></i> 打赏几块钱，让服务器君跑的更快些，谢谢大家！<button class="btn s2 fr" onclick="$('#alipay-form').submit();"><i class="fa fa-cny"> <?php _e('打赏支持');?></i></button></p>
 			<div class="post-donate-img">
 				<p>支付宝二维码</p>
 				<img src="<?php $this->options->themeUrl('img/alipay.png'); ?>">
